fed08c1adf9ba76af93fd84aa9b2c5e857bd6a26
Kirapan <pxqpxqpxq@hotmail.com>
2018-03-19 23:33:12 +0000

    step 9&10

--- download_avatars.js
+++ download_avatars.js
@@ -3,8 +3,14 @@ var token = require('./secret.js');
 var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  if (!repoOwner || !repoName) {
+    console.log("Input error. Please check repoowner and reponame.");
+    return;
+  }
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -20,7 +26,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
   console.log("Errors:", err);
   result = result.map(function (element) {
     downloadImageByURL(element.avatar_url,`./avatars/${element.login}.jpg`);
